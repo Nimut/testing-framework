a5f6e2a854fc34af224ca2ad9e0751bd04444e11
Nicole Cordes <typo3@cordes.co>
2017-02-16 12:55:14 +0100

    [TASK] Mention TYPO3 CMS project in license header

--- src/TestingFramework/TestCase/BaseTestCase.php
+++ src/TestingFramework/TestCase/BaseTestCase.php
@@ -4,6 +4,8 @@ namespace Nimut\TestingFramework\TestCase;
 /*
  * This file is part of the NIMUT testing-framework project.
  *
+ * It was taken from the TYPO3 CMS project (www.typo3.org).
+ *
  * It is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License, either version 2
  * of the License, or any later version.
