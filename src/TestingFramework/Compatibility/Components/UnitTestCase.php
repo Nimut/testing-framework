0d1a345dc5bd22d561995640111df0383b5062c5
Nicole Cordes <typo3@cordes.co>
2017-02-16 03:08:54 +0100

    [FEATURE] Add missing compatibility class for TYPO3 CMS 8.6

---
+++ src/TestingFramework/Compatibility/Components/UnitTestCase.php
@@ -0,0 +1,17 @@
+<?php
+namespace TYPO3\Components\TestingFramework\Core\Unit;
+
+/*
+ * This file is part of the NIMUT testing-framework project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read
+ * LICENSE file that was distributed with this source code.
+ */
+
+abstract class UnitTestCase extends \Nimut\TestingFramework\TestCase\UnitTestCase
+{
+}
