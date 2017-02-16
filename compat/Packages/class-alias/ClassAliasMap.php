533da274c75c15491b8218953705ab67f4ff9ab9
Nicole Cordes <typo3@cordes.co>
2017-02-16 12:25:01 +0100

    [TASK] Provide TYPO3\Components\TestingFramework classes for dev only

---
+++ compat/Packages/class-alias/ClassAliasMap.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'TYPO3\\Components\\TestingFramework\\Core\\Exception' => \Nimut\TestingFramework\Exception\Exception::class,
+    'TYPO3\\Components\\TestingFramework\\Core\\FileStreamWrapper' => \Nimut\TestingFramework\File\FileStreamWrapper::class,
+    'TYPO3\\Components\\TestingFramework\\Core\\Unit\\UnitTestCase' => \Nimut\TestingFramework\TestCase\UnitTestCase::class,
+    'TYPO3\\Components\\TestingFramework\\Fluid\\Unit\\ViewHelpers\\ViewHelperBaseTestcase' => \Nimut\TestingFramework\TestCase\ViewHelperBaseTestcase::class,
+];
