3ec50b3c140bd1140571ccc3d7ff24a0e8999859
Nicole Cordes <typo3@cordes.co>
2017-02-16 03:17:59 +0100

    [FEATURE] Add Exception classes

---
+++ src/TestingFramework/Exception/Exception.php
@@ -0,0 +1,21 @@
+<?php
+namespace Nimut\TestingFramework\Exception;
+
+/*
+ * This file is part of the NIMUT testing-framework project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read
+ * LICENSE file that was distributed with this source code.
+ */
+
+/**
+ * An exception - Thrown in abstract test cases to mark
+ * a test configuration or setup error.
+ */
+class Exception extends \Exception
+{
+}
