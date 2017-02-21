43a83e8816bd0cd8f779876b231e6ba761927cf7
Nicole Cordes <typo3@cordes.co>
2017-02-21 10:55:42 +0100

    [TASK] Add basic extension files

---
+++ tests/Packages/testbase/ext_emconf.php
@@ -0,0 +1,40 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "testbase".
+ *
+ * Auto generated 21-02-2017 10:54
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'Testbase extension',
+  'description' => 'Testbase extension for nimut/testing-framework',
+  'category' => 'misc',
+  'author' => 'Nicole Cordes, Helmut Hummel',
+  'author_email' => 'cordes@cps-it.de, info@helhum.io',
+  'author_company' => '',
+  'state' => 'stable',
+  'version' => '0.1.0',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'clearCacheOnLoad' => 0,
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '6.2.0-8.6.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  '_md5_values_when_last_written' => 'a:2:{s:13:"composer.json";s:4:"1e31";s:12:"ext_icon.png";s:4:"d6a7";}',
+);
+
