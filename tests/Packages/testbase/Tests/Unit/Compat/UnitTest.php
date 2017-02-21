dfa1a608d4178f1495033a99db028687be8831de
Nicole Cordes <typo3@cordes.co>
2017-02-21 11:01:13 +0100

    [FEATURE] Add basic UnitTest files

---
+++ tests/Packages/testbase/Tests/Unit/Compat/UnitTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Nimut\Testbase\Tests\Unit\Compat;
+
+/*
+ * This file is part of the NIMUT testing-framework project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read
+ * LICENSE file that was distributed with this source code.
+ */
+
+use TYPO3\CMS\Core\Tests\UnitTestCase;
+
+class UnitTest extends UnitTestCase
+{
+    /**
+     * @test
+     */
+    public function unitTestIsSetUp()
+    {
+        $this->assertTrue(true);
+    }
+}
