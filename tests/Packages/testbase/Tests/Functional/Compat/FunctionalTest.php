c3009ecb4076b58f35ee101cc05f9b13ae1c2d14
Nicole Cordes <typo3@cordes.co>
2017-02-21 18:52:07 +0100

    [FEATURE] Add functional test classes and ensure backend user login

---
+++ tests/Packages/testbase/Tests/Functional/Compat/FunctionalTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace Nimut\Testbase\Tests\Functional\Compat;
+
+/*
+ * This file is part of the NIMUT testing-framework project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read
+ * LICENSE file that was distributed with this source code.
+ */
+
+use TYPO3\CMS\Core\Tests\FunctionalTestCase;
+
+class FunctionalTest extends FunctionalTestCase
+{
+    /**
+     * @test
+     */
+    public function adminUserIsLoggedIn()
+    {
+        $backendUser = $this->setUpBackendUserFromFixture(1);
+
+        $this->assertTrue($backendUser->isAdmin());
+    }
+}
