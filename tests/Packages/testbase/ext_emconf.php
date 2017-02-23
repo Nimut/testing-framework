4e04fa5596f9b1458dbdbe62999a3e6f77895035
Nicole Cordes <typo3@cordes.co>
2017-02-23 11:28:49 +0100

    [TASK] Set testbase version to 0.5.0

--- tests/Packages/testbase/ext_emconf.php
+++ tests/Packages/testbase/ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "testbase".
  *
- * Auto generated 23-02-2017 01:20
+ * Auto generated 23-02-2017 11:28
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = array (
   'author_email' => 'cordes@cps-it.de, info@helhum.io',
   'author_company' => '',
   'state' => 'stable',
-  'version' => '0.4.0',
+  'version' => '0.5.0',
   'uploadfolder' => 0,
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
@@ -35,6 +35,6 @@ $EM_CONF[$_EXTKEY] = array (
     array (
     ),
   ),
-  '_md5_values_when_last_written' => 'a:12:{s:13:"composer.json";s:4:"2e0b";s:12:"ext_icon.png";s:4:"d6a7";s:16:"Classes/Mock.php";s:4:"6a0a";s:48:"Classes/ViewHelpers/RenderChildrenViewHelper.php";s:4:"a4c8";s:35:"Tests/Functional/FunctionalTest.php";s:4:"e46a";s:42:"Tests/Functional/Compat/FunctionalTest.php";s:4:"36a4";s:23:"Tests/Unit/MockTest.php";s:4:"eac8";s:23:"Tests/Unit/UnitTest.php";s:4:"97dd";s:30:"Tests/Unit/Compat/MockTest.php";s:4:"832d";s:30:"Tests/Unit/Compat/UnitTest.php";s:4:"772c";s:62:"Tests/Unit/Compat/ViewHelpers/RenderChildrenViewHelperTest.php";s:4:"8afa";s:55:"Tests/Unit/ViewHelpers/RenderChildrenViewHelperTest.php";s:4:"9445";}',
+  '_md5_values_when_last_written' => 'a:15:{s:13:"composer.json";s:4:"2e0b";s:12:"ext_icon.png";s:4:"d6a7";s:16:"Classes/Mock.php";s:4:"6a0a";s:48:"Classes/ViewHelpers/RenderChildrenViewHelper.php";s:4:"a4c8";s:35:"Tests/Functional/FunctionalTest.php";s:4:"e46a";s:42:"Tests/Functional/Compat/FunctionalTest.php";s:4:"36a4";s:49:"Tests/Functional/Fixtures/Templates/Template.html";s:4:"01df";s:44:"Tests/Functional/Fixtures/TypoScript/Page.ts";s:4:"3109";s:61:"Tests/Functional/ViewHelpers/RenderChildrenViewHelperTest.php";s:4:"8c31";s:23:"Tests/Unit/MockTest.php";s:4:"eac8";s:23:"Tests/Unit/UnitTest.php";s:4:"97dd";s:30:"Tests/Unit/Compat/MockTest.php";s:4:"832d";s:30:"Tests/Unit/Compat/UnitTest.php";s:4:"772c";s:62:"Tests/Unit/Compat/ViewHelpers/RenderChildrenViewHelperTest.php";s:4:"8afa";s:55:"Tests/Unit/ViewHelpers/RenderChildrenViewHelperTest.php";s:4:"9445";}',
 );
 
