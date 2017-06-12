84266295e4db0afd8da44bd95dbae4f6c7f90934
Robert Pupel <pupel@comsolit.com>
2017-06-12 17:29:49 +0200

    Allow setting relative TYPO3_PATH_WEB path in .env file

--- src/TestingFramework/Bootstrap/AbstractBootstrap.php
+++ src/TestingFramework/Bootstrap/AbstractBootstrap.php
@@ -142,7 +142,9 @@ abstract class AbstractBootstrap
         if (getenv('TYPO3_PATH_ROOT')) {
             $webRoot = getenv('TYPO3_PATH_ROOT');
         } elseif (getenv('TYPO3_PATH_WEB')) {
-            $webRoot = getenv('TYPO3_PATH_WEB');
+            $webRoot = preg_match('/\.\//', getenv('TYPO3_PATH_WEB'))
+                ? getcwd() . '/' . getenv('TYPO3_PATH_WEB')
+                : getenv('TYPO3_PATH_WEB');
         } else {
             $webRoot = getcwd();
         }
