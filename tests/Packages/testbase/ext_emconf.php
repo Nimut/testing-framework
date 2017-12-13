b990079553779d595b8de420d5d938da78962e0c
Nicole Cordes <typo3@cordes.co>
2017-12-13 18:12:32 +0100

    [FEATURE] Upgrade PHP and PHPUnit dependencies

--- tests/Packages/testbase/ext_emconf.php
+++ tests/Packages/testbase/ext_emconf.php
@@ -22,16 +22,16 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => 0,
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'constraints' => 
+  'constraints' =>
   array (
-    'depends' => 
+    'depends' =>
     array (
-      'typo3' => '6.2.0-8.6.99',
+      'typo3' => '7.6.0-9.0.99',
     ),
-    'conflicts' => 
+    'conflicts' =>
     array (
     ),
-    'suggests' => 
+    'suggests' =>
     array (
     ),
   ),
