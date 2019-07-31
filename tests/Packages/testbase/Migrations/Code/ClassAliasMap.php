29234858907e3ff9faad65b17b27be91d46f76ae
Sven Carstens <sven.carstens@aoe.com>
2019-07-31 14:38:14 +0200

    Revert "[Mod] Use ClassAliasMap to support old TYPO3/CMS/Fluid"
    This reverts commit 896ec2f3d65f329aa2a50dd15bb1cb8213fc28c4.

--- tests/Packages/testbase/Migrations/Code/ClassAliasMap.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    // Base classes removed in TYPO3 v9
-    'TYPO3\\CMS\\Fluid\\Core\\ViewHelper\\AbstractViewHelper' => \TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper::class,
-    'TYPO3\\CMS\\Fluid\\Core\\ViewHelper\\Facets\\CompilableInterface' => \TYPO3Fluid\Fluid\Core\ViewHelper\ViewHelperInterface::class,
-];
